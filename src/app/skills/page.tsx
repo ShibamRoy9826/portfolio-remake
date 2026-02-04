6d1653ceff357bfe7dd1386f2f1c66e12140664a
Shibam Roy <royshibam9826@gmail.com>
2026-02-04 23:00:48 +0530

    Updated data

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -19,13 +19,11 @@ interface SkillType{
     id:string;
     img:string;
     title:string;
-    progress:number;
-    currLearn:boolean;
 }
 
 let data;
 function Skills(){
-    const [skillsData,setSkills]=useState<{langs:SkillType[],fields:SkillType[],frameworks:SkillType[]}|null>(null);
+    const [skillsData,setSkills]=useState<{stack:SkillType[]}|null>(null);
     useEffect(()=>{
         const getSkills=async () =>{
             data=await fetchSkills();
@@ -39,28 +37,20 @@ function Skills(){
 
     return(
         <div className="flex items-center justify-center w-full h-full my-28">
-            <div className="w-[90%] h-full flex-col grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div className="w-[90%] h-full flex">
                 <div className="flex flex-col items-center rounded-xl p-12 justify-center border-3 border-[var(--bg3)]"
                 style={{backgroundImage:"linear-gradient(120deg,var(--bg2) 10%,var(--bg) 100%)"}}>
-                    <h1 className={`${silkscreen.className} text-center text-2xl mb-4`}>Programming Languages</h1>
-                    <ul className="p-4">
+                    <h1 className={`${silkscreen.className} text-center text-2xl mb-4`}>Current Skillset</h1>
+                    <ul className="p-4 grid grid-cols-8">
                         {
                             skillsData?(
-                                skillsData?.langs.map((l:SkillType,i:number)=>(
+                                skillsData?.stack.map((l:SkillType,i:number)=>(
                                     <li key={i} className="flex flex-col items-center justify-center">
                                         <div className="flex flex-row items-center justify-center">
-                                            <Image src={l.img} alt={l.title} height={30} width={30}/>
+                                            <Image src={l.img} alt={l.title} height={50} width={50}/>
                                             <p className="flex items-center justify-center m-4  text-center text-[var(--fg2)] text-lg">{l.title}
                                             </p>
-                                            {l.currLearn && (
-                                                <motion.div
-                                                animate={{opacity:[0,1,0]}}
-                                                transition={{repeat:Infinity,repeatType:"loop",type:"tween",duration:2,ease:"easeInOut"}}>
-                                                    <span className="mx-4 text-center text-[var(--secondary)] text-md">[In Progress]</span>
-                                                </motion.div>
-                                            )}
                                         </div>
-                                        <ProgressBar full={(window.innerWidth>768)?"35vw":"80vw"} percent={l.progress} secondary={l.currLearn}/>
                                     </li>
                                 ))
                         ):
@@ -71,72 +61,6 @@ function Skills(){
                     
                     }
 
-                    </ul>
-                </div>
-                <div className="flex flex-col items-center rounded-xl p-12 justify-center border-3 border-[var(--bg3)]"
-                style={{backgroundImage:"linear-gradient(120deg,var(--bg2) 0%,var(--bg) 100%)"}}>
-                    <h1 className={`${silkscreen.className} text-center text-2xl mb-4`}>Fields of interest</h1>
-                    <ul>
-                        {
-                            skillsData?(
-                                skillsData?.fields.map((f:SkillType,i:number)=>(
-                                    <li key={i} className="flex flex-col items-center justify-center">
-                                        <div className="flex flex-row items-center justify-center">
-                                            <Image src={f.img} alt={f.title} height={40} width={40}/>
-                                            <p className="m-4 text-center text-[var(--fg2)] text-lg">{f.title}</p>
-
-                                            {f.currLearn && (
-                                                <motion.div
-                                                animate={{opacity:[0,1,0]}}
-                                                transition={{repeat:Infinity,repeatType:"loop",type:"tween",duration:2,ease:"easeInOut"}}>
-                                                    <span className="m-4 text-center text-[var(--secondary)] text-md">[In Progress]</span>
-                                                </motion.div>
-                                            )}
-                                        </div>
-
-                                        <ProgressBar full={(window.innerWidth>768)?"35vw":"80vw"} percent={f.progress} secondary={f.currLearn}/>
-                                    </li>
-                                ))):
-
-                        <motion.div className="flex items-center justify-center rounded-xl h-[18vh] p-24 w-full"
-                        style={{backgroundColor:"rgba(255,255,255,0.05)"}} initial={{opacity:1}} animate={{opacity:[0,1,0]}} transition={{duration:1,repeat:Infinity,repeatType:"loop"}}>
-                            <p className="text-center text-[var(--fg2)]"><i>Items Loading...</i></p>
-                        </motion.div>
-                            }
-                    </ul>
-                </div>
-                <div className="col-span-1 md:col-span-2 flex flex-col items-center rounded-xl p-12 justify-center border-3 border-[var(--bg3)]"
-                style={{
-                    backgroundImage:"linear-gradient(120deg,var(--bg2) 10%,var(--bg) 100%)"
-                }}>
-                    <h1 className={`${silkscreen.className} text-center text-2xl mb-4`}>Rest of my Tech Stack</h1>
-                    <ul className="w-full grid grid-cols-1 md:grid-cols-5">
-                        {
-                            skillsData? (
-                                skillsData?.frameworks.map((f:SkillType,i:number)=>(
-                                    <li key={i} className="flex flex-col items-center justify-center mt-4">
-                                        <div className="flex flex-row items-center justify-center">
-                                            <Image src={f.img} alt={f.title} height={30} width={30}/>
-                                            <p className="m-4 text-center text-[var(--fg2)] text-lg">{f.title}</p>
-
-                                            {f.currLearn && (
-                                                <motion.div
-                                                animate={{opacity:[0,1,0]}}
-                                                transition={{repeat:Infinity,repeatType:"loop",type:"tween",duration:2,ease:"easeInOut"}}>
-                                                    <span className="m-4 text-center text-[var(--secondary)] text-md">[In Progress]</span>
-                                                </motion.div>
-                                            )}
-                                        </div>
-
-                                        <ProgressBar full={(window.innerWidth>768)?"10vw":"80vw"} percent={f.progress} secondary={f.currLearn}/>
-                                    </li>
-                                ))):
-
-                        <motion.div className="flex items-center justify-center rounded-xl h-[18vh] p-24 w-full md:col-span-5"
-                        style={{backgroundColor:"rgba(255,255,255,0.05)"}} initial={{opacity:1}} animate={{opacity:[0,1,0]}} transition={{duration:1,repeat:Infinity,repeatType:"loop"}}>
-                            <p className="text-center text-[var(--fg2)]"><i>Items Loading...</i></p>
-                        </motion.div>
-                            }
                     </ul>
                 </div>
 
